644a331710022eb6cf0beaf5c8b002facb5af133
SYJ <subutuo@naver.com>
2021-02-08 17:33:39 +0900

    static 경로 설정

--- source/esm/esm/settings.py
+++ source/esm/esm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 
     'authApp',
     # esm_sys 디렉토리 하위 apps 등록
@@ -136,3 +138,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# static 경로에 있는 파일을 public하게 오픈
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
