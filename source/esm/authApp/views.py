3f35ecd3db28453e309ed2bbe491f1386d8893c3
dims1111 <dims1111@gmail.com>
2021-02-09 14:07:08 +0900

    autoApp views.py update

--- source/esm/authApp/views.py
+++ source/esm/authApp/views.py
@@ -1,69 +1,69 @@
-from django.shortcuts import render,redirect
-import cx_Oracle
-#한글 지원 방법
+# 장고 프렘임워크에서 템플릿 렌더링 및 URL 리다이렉트
+from django.shortcuts import render, redirect
 
+# 단일 트랜잭션 처리를 위한 패키지 임포트
+from django.db import transaction
+
+# 오라클 접속하기 위한 패키지
+import cx_Oracle
+
+# 시스템 변수 설정을 위한 패키지
 import os
+
+# 한글 지원 방법
 os.putenv('NLS_LANG', '.UTF8')
 
-# Create your views here.
-
+# login 함수
+@transaction.atomic
 def login(request):
-
-  if request.method == 'POST':
-
-    # 입력값 가져오기
-    userid = request.POST['userid']
-    in_password = request.POST['password']
-
-
-    # DB연결
-    connection = cx_Oracle.connect('esm/esm@210.112.232.29:1531/vis1226')
-
-    # 커서생성
-    cursor = connection.cursor()
-
-    # 쿼리문입력(입력받은 아이디와 같은 아이디의 패스워드 값 가져오기)
-    cursor.execute("""
-    select password 
-    from sys_user 
-    where user_account =: user_account""",
-    user_account = userid
-    )
-
-
+	if request.method == 'POST':
     
-    password_o = ""
+	# 입력값 가져오기
+    srchUserAccount = request.POST['userid']
+    srchPassword = request.POST['password']
 
-    # 결과값 가져오기
-    for password in cursor:
+	# DB연결
+	connection = cx_Oracle.connect('esm/esm@210.112.232.29:1531/vis1226')
 
-      # 변수에 결과값 넣어주기
-      password_o = password[0]
+	# 커서생성
+	cursor = connection.cursor()
+	
+	# 테이블에 존재하는 비밀번호
+	vPassword = ""
 
+    try:
+      	# 쿼리문입력(입력받은 아이디와 같은 아이디의 패스워드 값 가져오기)
+      	cursor.execute( \
+      	"""
+        	select su.password 
+              from esm.sys_user su
+        	 where 1=1
+          	   and su.user_account = :user_account
+      	""", user_account = srchUserAccount
+      	)          	
 
+      	# 결과값 가져오기
+      	for password in cursor:
+        	# 변수에 결과값 넣어주기
+        	vPassword = password[0]
 
-    print(password_o, "=", in_password)
+      	print(vPassword, "=", srchPassword)
 
-    # 쿼리문을 통해 가져온 데이터베이스와 
-    if password_o == in_password:
+      	# 쿼리문을 통해 가져온 데이터베이스와 
+      	if vPassword == srchPassword:
+        	print('로그인에 성공')
+        	return render(request, 'test.html')
+      	else:
+			print('로그인에 실패')
+        	return render(request, 'login.html')
 
-      print('로그인성공')
-
-      cursor.close()
-      connection.close()
-      return render(request, 'test.html')
-
+    # POST방식이 아닐 시
     else:
-
-
-      cursor.close()
-      connection.close()
-      print('로그인실패')
-      return render(request, 'login.html')
-
-  # POST방식이 아닐 시
-  else:
-
-    return render(request, 'login.html')
-
-  
+      	return render(request, 'login.html')
+  	except Exception as e:
+    	print(e)
+  	else:
+    	print("정상적으로 로그인 하였습니다.")
+  	finally:
+		cursor.close()
+		connection.close()	  	
