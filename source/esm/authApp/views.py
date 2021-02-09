1e54c397592f7debab20ef17b92a65ddef430d56
dims1111 <dims1111@gmail.com>
2021-02-09 14:28:01 +0900

    views.py 수정

--- source/esm/authApp/views.py
+++ source/esm/authApp/views.py
@@ -16,54 +16,51 @@ os.putenv('NLS_LANG', '.UTF8')
 # login 함수
 @transaction.atomic
 def login(request):
-	if request.method == 'POST':
-    
-	# 입력값 가져오기
-    srchUserAccount = request.POST['userid']
-    srchPassword = request.POST['password']
 
-	# DB연결
-	connection = cx_Oracle.connect('esm/esm@210.112.232.29:1531/vis1226')
+	# POST 방식
+	if request.method == 'GET':
+		return render(request, 'login.html')
+	# GET 방식
+	else:
+		# 입력값 가져오기
+		srchUserAccount = request.POST['userid']
+		srchPassword = request.POST['password']
 
-	# 커서생성
-	cursor = connection.cursor()
+		# 테이블에 존재하는 비밀번호
+		vPassword = ""
+
+		connectionDns = 'esm/esm@210.112.232.29:1531/vis1226'
+
+		try:
+			# DB연결, 커서생성
+			with cx_Oracle.connect(connectionDns) as connection, connection.cursor() as cursor:
+				# 쿼리문입력(입력받은 아이디와 같은 아이디의 패스워드 값 가져오기)
+				cursor.execute( \
+				"""
+					select su.password 
+					from esm.sys_user su
+					where 1=1
+					and su.user_account = :user_account
+				""", user_account = srchUserAccount
+				)          	
+
+				# 결과값 가져오기
+				for password in cursor:
+					# 변수에 결과값 넣어주기
+					vPassword = password[0]
+
+				print(vPassword, "=", srchPassword)
+
+				# 쿼리문을 통해 가져온 데이터베이스와 
+				if vPassword == srchPassword:
+					print('로그인에 성공')
+					return render(request, 'test.html')
+				else:
+					print('로그인에 실패')
+					return render(request, 'login.html')
+
+		except Exception as e:
+			print(e)
+		else:
+			print("정상적으로 로그인 하였습니다. 이제 메인 화면을 작성하시기 바랍니다.")
 	
-	# 테이블에 존재하는 비밀번호
-	vPassword = ""
-
-    try:
-      	# 쿼리문입력(입력받은 아이디와 같은 아이디의 패스워드 값 가져오기)
-      	cursor.execute( \
-      	"""
-        	select su.password 
-              from esm.sys_user su
-        	 where 1=1
-          	   and su.user_account = :user_account
-      	""", user_account = srchUserAccount
-      	)          	
-
-      	# 결과값 가져오기
-      	for password in cursor:
-        	# 변수에 결과값 넣어주기
-        	vPassword = password[0]
-
-      	print(vPassword, "=", srchPassword)
-
-      	# 쿼리문을 통해 가져온 데이터베이스와 
-      	if vPassword == srchPassword:
-        	print('로그인에 성공')
-        	return render(request, 'test.html')
-      	else:
-			print('로그인에 실패')
-        	return render(request, 'login.html')
-
-    # POST방식이 아닐 시
-    else:
-      	return render(request, 'login.html')
-  	except Exception as e:
-    	print(e)
-  	else:
-    	print("정상적으로 로그인 하였습니다.")
-  	finally:
-		cursor.close()
-		connection.close()	  	
