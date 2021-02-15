e6ff6056bb2b7769ee71f4a76da2e674d3084c1d
dims1111 <dims1111@gmail.com>
2021-02-15 15:20:09 +0900

    sys_menu_v 뷰 작성

---
+++ document/30.설계(DP)/DP-1060.테이블 설계/ESM_SYS_1010.메뉴/sql/트리 메뉴 조회.sql
@@ -0,0 +1,3 @@
+select *
+  from esm.sys_menu_v
+ where 1=1
---
+++ document/30.설계(DP)/DP-1060.테이블 설계/ESM_SYS_1010.메뉴/view/sys_menu_v.sql
@@ -0,0 +1,31 @@
+create or replace view esm.sys_menu_v
+as
+select lpad(' ', 2 * (level - 1)) || sm.menu_name_ko as menu_name_ko_dis
+      ,lpad(' ', 2 * (level - 1)) || sm.menu_name_en as menu_name_en_dis
+      ,level                                         as tree_level
+      ,sm.menu_id
+      ,sm.menu_name_ko
+      ,sm.menu_name_en
+      ,sm.url
+      ,sm.prent_menu_id
+      ,sm.icons
+      ,sm.sort_order
+      ,sm.use_yn
+      ,sm.search_yn
+      ,sm.insert_yn
+      ,sm.update_yn
+      ,sm.delete_yn
+      ,sm.print_yn
+      ,sm.batch_yn
+      ,sm.excel_down_yn
+      ,sm.excel_up_yn
+      ,sm.remark
+      ,sm.create_date_time
+      ,sm.create_by
+      ,sm.update_date_time
+      ,sm.update_by
+  from esm.sys_menu sm
+ where 1=1
+ start with sm.prent_menu_id = -1
+connect by prior sm.menu_id = sm.prent_menu_id
+ order siblings by sm.sort_order
