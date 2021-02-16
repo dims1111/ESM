6b6dd9c96538ef21d49136151c350af6c276d185
dims1111 <dims1111@gmail.com>
2021-02-16 10:05:18 +0900

    esm_app admin & models update

--- source/esm/esm_app/models.py
+++ source/esm/esm_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+# 로그인 앱의 모델내 SysUser 클래스 임포트
+from . models import SysUser
+
+
+
 # Create your models here.
+# 장고 Admin 데이터 조호시 그리드에 출력될 항목 정의
+class SysUserAdmin(admin.ModelAdmin):    
+    list_display = ('user_id', 'user_account', 'user_name', 'hp_number', 'email_addr', 'person_id')
+
+# 장고 Admin 등록
+admin.site.register(SysUser, SysUserAdmin)
