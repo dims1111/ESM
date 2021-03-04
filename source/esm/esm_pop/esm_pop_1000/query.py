d575ea287fd5b13dbb4ea62fefc1413110072efc
dims1111 <dims1111@gmail.com>
2021-03-04 13:54:18 +0900

    공통코드 조회 앱 추가

--- source/esm/esm_pop/esm_pop_1000/query.py
+++ source/esm/esm_pop/esm_pop_1000/query.py
@@ -15,16 +15,16 @@ class sql:
           select scm.code_master_uid                                               as code_master_uid
                 ,scd.code_detail_uid                                               as code_detail_uid
                 ,scm.master_cd                                                     as master_cd
-                ,case when :p_lang_cd = 'ko' then scm.name_ko else scm.name_en end as master_cd_name
+                ,case when &p_lang_cd = 'ko' then scm.name_ko else scm.name_en end as master_cd_name
                 ,scd.detail_cd                                                     as detail_cd
-                ,case when :p_lang_cd = 'ko' then scd.name_ko else scd.name_en end as detail_cd_name
+                ,case when &p_lang_cd = 'ko' then scd.name_ko else scd.name_en end as detail_cd_name
                 ,scd.sort_order                                                    as sort_order
                 ,to_char(scd.begin_date, 'yyyy-mm-dd')                             as begin_date
                 ,to_char(scd.end_date, 'yyyy-mm-dd')                               as end_date
             from esm.sys_code_master scm
                  inner join esm.sys_code_detail scd on scm.master_cd = scm.master_cd
            where 1=1
-             and scm.master_cd  = :p_master_cd
+             and scm.master_cd  = &p_master_cd
              and trunc(sysdate) between scm.begin_date and nvl(scm.end_date, to_date('4712-12-31', 'yyyy-mm-dd'))
            order by scd.sort_order
         """
--- source/esm/esm_pop/esm_pop_1000/views.py
+++ source/esm/esm_pop/esm_pop_1000/views.py
@@ -15,19 +15,20 @@ import cx_Oracle
 # 공통 쿼리 클랙스 임포트
 from . query import sql
 
+# query 실행을 위한 클래스 임포트
+from esm_com import views as esmComViews
 
-# 서브 메뉴 조회
+
+# 공통코드 조회
 def searchCodeDetal(dictParams):  
   try:
-    # DB연결, 커서생성
-    with cx_Oracle.connect(connDns) as connection, connection.cursor() as cursor:
-      # 쿼리 실행
-      # print('sqlString =>' ,sqlString, type(sqlString))
-      cursor.execute(sql.codeMasterDetail, :p_lang_cd=p_lang_cd, :p_master_cd=p_lang_cd)
+    dictParams = {}
+    dictParams["&p_lang_cd"] = 'ko'
 
-      # 쿼리 결과값 가져오기
-      return JsonResponse(rowsToDictList(cursor))
-  
-  except (cx_Oracle.DatabaseError, Exception) as e:
-    commParam = {'cd' : 'E', 'msg' : e}
-    return JsonResponse(commParam)
+    # 공통코드 조회
+    params = {}
+    params['resultData'] = esmComViews.searchExecute(sql.codeMasterDetail, dictParams)
+    return JsonResponse(params)
+    
+  except (Exception) as e:
+    return JsonResponse( {'cd' : 'E', 'msg' : str(e)} )
