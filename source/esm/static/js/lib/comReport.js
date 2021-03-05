3d0c02ff59addc1658eb74ef17d911252968ffe2
dims1111 <dims1111@gmail.com>
2021-03-05 23:24:39 +0900

    url 변경

--- source/esm/static/js/lib/comReport.js
+++ source/esm/static/js/lib/comReport.js
@@ -7,9 +7,9 @@
 function report_show(crfname, params) {
     var param_str = btoa(encodeURIComponent(params));
     var hostName = location.hostname;
-    var clipUrl = 'http://acadrs.kaist.ac.kr';
+    var clipUrl = 'http://acadrs.built1.com';
     if (hostName.indexOf('scholarprod') < 0) {
-      clipUrl = 'http://dacadrs.kaist.ac.kr';
+      clipUrl = 'http://dacadrs.built1.com';
     }
 
     console.log(clipUrl + '/clipreport4/sch_report.jsp?c=' + crfname + '&p=' + param_str, crfname);
@@ -17,7 +17,7 @@ function report_show(crfname, params) {
     // console.log("hostName...", hostName);  //localhost
     //window.open('//'+clipUrl+'/clipreport4/sti_report.jsp?c=' + crfname + '&p=' + param_str);
     window.top.open(clipUrl + '/clipreport4/sch_report.jsp?c=' + crfname + '&p=' + param_str, crfname);
-    //http://143.248.105.161/clipreport4/sti_report_dev.jsp?c=test&p=%EA%B5%AC%EB%B6%84
+    //http://127.0.0.1/clipreport4/sti_report_dev.jsp?c=test&p=%EA%B5%AC%EB%B6%84
   }
   
   /**
@@ -29,13 +29,13 @@ function report_show(crfname, params) {
 function report_show_newtab(crfname, params) {
   var param_str = btoa(encodeURIComponent(params));
   var hostName = location.hostname;
-  var clipUrl = 'http://acadrs.kaist.ac.kr';
+  var clipUrl = 'http://acadrs.built1.com';
   if (hostName.indexOf('scholarprod') < 0) {
-    clipUrl = 'http://dacadrs.kaist.ac.kr';
+    clipUrl = 'http://dacadrs.built1.com';
   }
   window.open(clipUrl + '/clipreport4/sch_report.jsp?c=' + crfname + '&p=' + param_str,'_blank');
 
   // console.log("hostName...", hostName);  //localhost
   //window.open('//'+clipUrl+'/clipreport4/sti_report.jsp?c=' + crfname + '&p=' + param_str);
-  //http://143.248.105.161/clipreport4/sti_report_dev.jsp?c=test&p=%EA%B5%AC%EB%B6%84
+  //http://127.0.0.1/clipreport4/sti_report_dev.jsp?c=test&p=%EA%B5%AC%EB%B6%84
 }
